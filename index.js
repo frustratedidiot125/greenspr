7f9c1433833dec1ad69a12684ecc5d03a5ed05ca
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2018-10-30 23:52:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ alexaApp.intent('StepIntent', {
 if (slotstep && slotstep > 0 && slotstep < 10 && parseFloat(slotstep) == parseInt(slotstep) && !isNaN(slotstep)){
   var step = slotstep; 
     } else if (slotstep == 0){
-    var step = 10;
+    var step = 1;
     
     } else if (slotstep > 9 ){
    var morethanseven = 1; 
